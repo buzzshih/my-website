3257d4e8aea7d9b83e0cd64c35be26936219910d
yuhsiang <H24364032@gmail.com>
2023-04-26 16:37:04 +0800

    disableSwitch theme set true

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,7 +103,7 @@ const config = {
           },
         ],
       },
-      colorMode: { defaultMode: "dark", disableSwitch: false },
+      colorMode: { defaultMode: "dark", disableSwitch: true },
       footer: {
         style: "dark",
         links: [
